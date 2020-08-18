478e5ab1e3fce8123f4c698fd8375650f5137482
Lyle Branzuela <lylebranz@gmail.com>
2020-08-18 17:37:57 +1200

    INITIAL COMMIT: 	- Added Packages: 		1) Express JS 		2) React-Router 		3) React-Bootstrap
    Signed-off-by: Lyle Branzuela <lylebranz@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+      <Container fluid className="App-header">
+        <p>Hello World Test!</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          - Group 10 Software Engineering
+        </a>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
