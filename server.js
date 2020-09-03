29af032e4b9dd27d66eedfc79bcf0127724e4a45
Lyle Branzuela <lylebranz@gmail.com>
2020-09-03 19:50:55 +1200

    > Server Setup
    Signed-off-by: Lyle Branzuela <lylebranz@gmail.com>

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.get('/static/js/:file', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/static/js/' + req.params.file));
+});
+
+app.get('/static/css/:file', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/static/css/' + req.params.file));
+});
+
+app.get('/:file', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/' + req.params.file));
+});
+
+
+app.listen(8080);
