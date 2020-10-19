06b30474bcc15ff5072ad2a20934738e125c0cb8
Lyle Branzuela <lylebranz@gmail.com>
2020-10-19 20:44:08 +1300

    > Heroku Fix Attempt #3
    Signed-off-by: Lyle Branzuela <lylebranz@gmail.com>

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Mock API for we-are-us MVP
 const production = "https://se-evidence-repo.herokuapp.com";
 const development = "http://localhost:8080";
-const url = process.env.NODE_ENV ? development : production;
+const url = process.env.NODE_ENV === "development" ? development : production;
 
 export default axios.create({
   baseURL: url,
